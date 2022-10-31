a338e2f4825b54e23fe58cf467e3b640451f053e
Dima <117075948+Dmitri28@users.noreply.github.com>
2022-10-31 13:25:48 +0200

    Project25
    Project25

---
+++ src/symbolarray/SymbolArray.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package symbolarray;
+
+/**
+ *
+ * @author admin
+ */
+public class SymbolArray {
+
+    /**
+     * @param args the command line arguments
+     */
+    @SuppressWarnings("empty-statement")
+    public static void main(String[] args) {
+10][10]=        char arr = null[
+{{'-','-','-','-','-','-','-','-','-','-'},{'-','-','-','-','-','-','-','-','-','-','-'}};
+        for(int i=0;i<arr.length;i++)
+          
+        System.out.println(arr[i]);
+                
+                }
+       
+    }
+
+
+    
+
